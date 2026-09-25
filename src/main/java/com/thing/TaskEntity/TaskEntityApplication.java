1e76fe3e9cf7a489527557eb962ddf66a5b8cdd8
thing <thingstosee6@gmail.com>
2026-09-25 19:46:34 +0545

    added taskCompletion mark

--- src/main/java/com/thing/TaskEntity/TaskEntityApplication.java
+++ src/main/java/com/thing/TaskEntity/TaskEntityApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ToDoListApplication {
+public class TaskEntityApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(ToDoListApplication.class, args);
+		SpringApplication.run(TaskEntityApplication.class, args);
 	}
 }
